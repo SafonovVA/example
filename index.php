b9b72b81b19b58487691018ffbea44a629b6e0a3
vladimir <safonov.open@gmail.com>
2020-01-16 01:09:16 +0600

    composer what???

--- index.php
+++ index.php
@@ -1,5 +1,3 @@
 <a href="news.php">News</a><br />
 <a href="static_page.php">Static Page</a>
 <?php
-echo  $_SERVER['DOCUMENT_ROOT'];
-
--- static_page.php
+++ static_page.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once __DIR__ . '/vendor/autoload.php';
 use classes\News;
 use classes\Page;
 use classes\StaticPage;
